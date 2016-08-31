65601065ec12d67f96d358d3b4a0c03ad957662f
Kenneth Johnson <kenpilot@gmail.com>
2016-08-31 16:19:48 -0700

    confgen changes

--- confgen_conf.py
+++ confgen_conf.py
@@ -8,36 +8,12 @@ import os
 is_staging = os.path.dirname(os.path.abspath(__file__)).endswith('-staging')
 print('Generating config files for ' + ['PRODUCTION','STAGING'][is_staging] + ' environment')
 
-if is_staging:
-	venv = '/home/'+unix_user+'/'+project_name+'-staging'
-	output_files = {
-		# nginx config for /etc/nginx/sites-available
-		'nginx.conf': {
-			# 'template': 'confgen_nginx.tmpl',
-			'venv': venv,
-			'domain': domain_name,
-			'port': 81,
-			'ssl': False,
-			'ssl_port': 444,
-			'ssl_key_bundle': venv + '/keys/fullchain.pem',
-			'ssl_private_key': venv + '/keys/privkey.pem',
-		},
-
-		# systemd service file for /ecc/systemd/system
-		'uwsgi.service': {
-			'template': 'confgen_uwsgi.tmpl',
-			'venv': venv,
-			'unix_user': unix_user,
-			'unix_group': unix_group,
-			'settings_override': 'system.settings.staging',
-		},
-	}
-
-else:
+if not is_staging:
+	# PRODUCTION SETTINGS
 	venv = '/home/'+unix_user+'/'+project_name
 	output_files = {
 		# nginx config for /etc/nginx/sites-available
-		'nginx.conf': {
+		project_name+'.conf': {
 			'template': 'confgen_nginx.tmpl',
 			'venv': venv,
 			'domain': domain_name,
@@ -51,10 +27,36 @@ else:
 		},
 
 		# systemd service file for /ecc/systemd/system
-		'uwsgi.service': {
+		project_name+'.service': {
 			'template': 'confgen_uwsgi.tmpl',
 			'venv': venv,
-			'unix_user': unix_user,
-			'unix_group': unix_group,
+			'user': unix_user,
+			'group': unix_group,
+		},
+	}
+
+else:
+	# STAGING SETTINGS
+	venv = '/home/'+unix_user+'/'+project_name+'-staging'
+	output_files = {
+		# nginx config for /etc/nginx/sites-available
+		project_name+'-staging.conf': {
+			'template': 'confgen_nginx.tmpl',
+			'venv': venv,
+			'domain': domain_name,
+			'port': 81,
+			'ssl': False,
+			'ssl_port': 444,
+			'ssl_key_bundle': venv + '/keys/fullchain.pem',
+			'ssl_private_key': venv + '/keys/privkey.pem',
+		},
+
+		# systemd service file for /ecc/systemd/system
+		project_name+'-staging.service': {
+			'template': 'confgen_uwsgi.tmpl',
+			'venv': venv,
+			'user': unix_user,
+			'group': unix_group,
+			'settings_override': 'system.settings.staging',
 		},
 	}
