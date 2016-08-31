eaaedea0e0e46b003d0810e489b48cb01d11563f
Kenneth Johnson <kenpilot@gmail.com>
2016-08-31 16:54:25 -0700

    Fixed permissions

--- fixperms.sh
+++ fixperms.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-	echo "This script should be run as root" 1>&2
-fi
-
 dir="$(dirname $0)"
 echo "Verifying executable scripts have +x..."
 chmod +x $dir/*.sh
+chmod +x $dir/confgen.py
 chmod +x $dir/*/manage.py
 
 exit 0
