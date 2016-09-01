954166b1282eb6f24943b115bea91fe5d79878dc
Kenneth Johnson <kenpilot@gmail.com>
2016-08-31 22:35:09 -0700

    Removed generate_secret_key note, since it's now in the README

--- src/system/settings/production.py
+++ src/system/settings/production.py
@@ -4,7 +4,6 @@ _CONFIG_FILE = "production"
 DEBUG = False
 
 # Import key from an external file, so it doesn't get included in version control
-# If you have generate_secret_key installed, you can regenerate a new one with this: python manage.py generate_secret_key system/settings/secretkey.txt
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'secretkey.txt')) as f:
 	SECRET_KEY = f.read().strip()
 
